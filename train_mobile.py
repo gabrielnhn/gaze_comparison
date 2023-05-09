011eebc24d84880866ec56b934734c349f039c3d
GABRIEL NASCARELLA HISHIDA DO NASCIMENTO <gnhn19@inf.ufpr.br>
2023-05-09 19:06:13 -0300

    i forgor <skull emoji>

--- train_mobile.py
+++ train_mobile.py
@@ -244,7 +244,7 @@ if __name__ == '__main__':
                     pitch_predicted_cpu = pitch_predicted_cpu*np.pi/180
                     yaw_predicted_cpu = yaw_predicted_cpu*np.pi/180
                     
-                    for p,y,pl,yl in zip(pitch_predicted_cpu,yaw_predicted_cpu,label_pitch_cont_gaze,label_yaw_cont_gaze):
+                    for p,y,pl,yl in zip(pitch_predicted_cpu,yaw_predicted_cpu,label_pitch_cpu,label_yaw_cpu):
                         avg_error_train += angular(gazeto3d([p,y]), gazeto3d([pl,yl]))
 
 
