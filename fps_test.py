358e6b7a5d5798bc73ece18f2fa1ece301e454a2
Gabriel Nascarella Hishida <ielnascarella@hotmail.com>
2023-05-31 13:34:08 -0300

    fix paths FPS test

--- fps_test.py
+++ fps_test.py
@@ -76,14 +76,14 @@ if __name__ == '__main__':
 
     vri = VRI_GazeNet()
     vri.name = "VRI"
-    saved_state_dict = torch.load("./models/VRI-180-May-27-LR1e-05-DEC1e-06-drop0.3-BATCH8-augment-CROSSENTROPY-False-11.07v-11.49t.pkl")
+    saved_state_dict = torch.load("../models/VRI-180-May-27-LR1e-05-DEC1e-06-drop0.3-BATCH8-augment-CROSSENTROPY-False-11.07v-11.49t.pkl")
     vri.load_state_dict(saved_state_dict)
 
 
     l2cs = L2CS(torchvision.models.resnet.Bottleneck, [3, 4, 6, 3], 90)
     l2cs.num_bins = 90
     l2cs.name = "L2CS"
-    saved_state_dict = torch.load("./models/L2CSNet_gaze360.pkl")
+    saved_state_dict = torch.load("../models/L2CSNet_gaze360.pkl")
     model.load_state_dict(saved_state_dict)
 
     # TEST
